b47efa1e4e86aa41f6e647bbef3f85d71d3fa8fd
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-04-05 22:49:57 -0300

    feat: fetch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
-import { Header } from './components/header';
+
+import { Header } from '@/components/header';
 import './globals.css';
 
 export const metadata: Metadata = {
